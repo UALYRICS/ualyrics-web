5348b5db02f0f4ba9101a7806f4f4bb05fec68fe
Vitalii Kravets <vitalii.kravets@gmail.com>
2021-01-11 22:31:31 +0100

    Fix genius api key

--- src/service/search-service.ts
+++ src/service/search-service.ts
@@ -2,6 +2,6 @@ import Genius from "genius-lyrics";
 import {Song} from '../model/song';
 
 export const search = async (term: string) => {
-  const Client = new Genius.Client('_NqU5X3ywTnBAzvmVglDNeMDPiYBdsNGOEnmAcRKJO-P2nhMtDXz9MSoUbyfR7CZ'); // Scrapes if no key is provided
+  const Client = new Genius.Client('aH2LpSO9r9gVRGsxv-eazJoBu7ka8FRhp-lTUPeJRqVkOUy5W13oApk3ds67HZBpiLEs2j8Bln-jfagNKYn_sg'); // Scrapes if no key is provided
   return (await Client.songs.search(term)).map(s => ({name: s.title, geniusId: s.id} as Song));
 }
