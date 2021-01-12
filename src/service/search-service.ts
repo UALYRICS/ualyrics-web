d3e08f1eba9d7fa42693673b0e45168dcc1db6e6
Vitalii Kravets <vitalii.kravets@gmail.com>
2021-01-12 18:13:13 +0100

    Fix genius API-Key. Try III.

--- src/service/search-service.ts
+++ src/service/search-service.ts
@@ -2,6 +2,6 @@ import Genius from "genius-lyrics";
 import {Song} from '../model/song';
 
 export const search = async (term: string) => {
-  const Client = new Genius.Client('aH2LpSO9r9gVRGsxv-eazJoBu7ka8FRhp-lTUPeJRqVkOUy5W13oApk3ds67HZBpiLEs2j8Bln-jfagNKYn_sg'); // Scrapes if no key is provided
+  const Client = new Genius.Client('i6skCYqy5w6sNOHpW32r436jn9NoQPzBikS3mAXeMQnfbJh1hg-i8y7nIPhF4oe7'); // Scrapes if no key is provided
   return (await Client.songs.search(term)).map(s => ({name: s.title, geniusId: s.id} as Song));
 }
