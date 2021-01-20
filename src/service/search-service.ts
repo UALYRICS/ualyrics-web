3250dc92aaec43af2a416a79511e4635313cf087
Vitalii Kravets <vitalii.kravets@gmail.com>
2021-01-20 22:55:50 +0100

    Fix genius api host

--- src/service/search-service.ts
+++ src/service/search-service.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 
 export const search = async (term: string): Promise<Array<Song>> => {
   const key = 'i6skCYqy5w6sNOHpW32r436jn9NoQPzBikS3mAXeMQnfbJh1hg-i8y7nIPhF4oe7';
-  const response = await axios.get(`${process.env.REACT_APP_GENIUS_API_URL}/api/search?q=${term}`, {
+  const url = process.env.REACT_APP_GENIUS_API_URL ? process.env.REACT_APP_GENIUS_API_URL : '';
+  const response = await axios.get(`${url}/api/search?q=${term}`, {
     headers: {
         'Authorization': `Bearer ${key}`
     }
