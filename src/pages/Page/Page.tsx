537d9587227cf65cae2c552616365f294ae4f9ef
Vitalii Kravets <vitalii.kravets@gmail.com>
2021-04-18 13:16:13 +0200

    Add footer

--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { TopToolbar } from './TopToolbar';
 import { SocialToolbar } from './SocialToolbar';
+import { Footer } from './Footer';
 
 export const Page: FunctionComponent<{}> = ({ children }) => (
   <div className="container full-width">
     <TopToolbar />
     <SocialToolbar />
     {children}
+    <Footer />
   </div>
 );
