f64bd524e5b1c6baf9b5e31b9fa6d1809aa71b8d
Vitalii Kravets <vitalii.kravets@gmail.com>
2021-05-12 08:55:05 +0200

    Make Sign-In page responsive

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -10,11 +10,11 @@ export const LoginPage : FunctionComponent<{}> = () => {
 
   return (
     <div className="row">
-      <div className="col-6 my-auto">
+      <div className="col-12 col-lg-6 my-auto">
         <h3 className="left-block-title">UALYRICS</h3>
         <p className="left-block-text">Найамбітніший проект з перекладу текстів пісень українською!</p>
       </div>
-      <div className="col-6">
+      <div className="col-12 col-lg-6">
         <div className="card">
           <div className="card-body">
             <div className="card-title float-right">
