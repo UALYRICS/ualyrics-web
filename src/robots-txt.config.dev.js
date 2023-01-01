ad8c684e5eb40b8f97abeafb37f33db9a75b9dd4
Vitalii Kravets <vitalii.kravets@gmail.com>
2023-01-01 13:27:06 +0100

    Add robots.txt generation

---
+++ src/robots-txt.config.dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  policy: [
+    {
+      userAgent: "*",
+      disallow: "/",
+    },
+  ],
+  sitemap: "https://www.dev.ualyrics.com/sitemap.xml",
+  host: "https://www.dev.ualyrics.com",
+};
